f982653a1873c3234e9fba46db22902a24ff6277
sainimu78 <393584307@qq.com>
2024-07-28 00:42:15 +0800

    -feature:gdb调试测试

--- Source/Editor/Main.cpp
+++ Source/Editor/Main.cpp
@@ -1,16 +1,19 @@
+#include <stdio.h>
 #include "Engine.h"
-#include "Windows.h"
+//#include "Windows.h"
 
 typedef void (*FuncTestAPI_C)();
 
 int main(int argc, char** argv)
 {
 	TestAPI_Cpp();
-	auto hLib = LoadLibrary("Engine.dll");
-	if (hLib != NULL)
-	{
-		if (auto Func = reinterpret_cast<FuncTestAPI_C>(GetProcAddress(hLib, "TestAPI_C")))
-			Func();
-	}
+	//auto hLib = LoadLibrary("Engine.dll");
+	//if (hLib != NULL)
+	//{
+	//	if (auto Func = reinterpret_cast<FuncTestAPI_C>(GetProcAddress(hLib, "TestAPI_C")))
+	//		Func();
+	//}
+	printf("Finished\n");
+	fflush(stdout);
 	return 0;
 }
--- Source/Engine/Engine.cpp
+++ Source/Engine/Engine.cpp
@@ -1,11 +1,13 @@
+#include <stdio.h>
 #include "Engine.h"
-#include "stdio.h"
 
 void TestAPI_Cpp()
 {
 	printf("Invoke C++ API\n");
+	fflush(stdout);
 }
 void TestAPI_C()
 {
 	printf("Invoke C API\n");
+	fflush(stdout);
 }
