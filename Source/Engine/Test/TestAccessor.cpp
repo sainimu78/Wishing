feabedfffc179ba528de12e986277cbd3e625bd1
sainimu78 <393584307@qq.com>
2024-08-04 19:11:31 +0800

    -feature:transform加到测试类和数组中测试

--- Source/Engine/Test/TestAccessor.cpp
+++ Source/Engine/Test/TestAccessor.cpp
@@ -189,115 +189,127 @@ namespace Engine
 	};
 }
 
-namespace Engine
+template <typename T>
+class TMyVector
 {
-	template <typename T>
-	class TMyVector
-	{
-	public:
-		T m_x;
-		T m_y;
-		T m_z;
-	};
+public:
+	//TMyVector()
+	//	: m_x(0)
+	//	, m_y(0)
+	//	, m_z(0)
+	//{
 
-	template <typename T>
-	class TMyQuaternion
-	{
-	public:
-		T m_x;
-		T m_y;
-		T m_z;
-		T m_w;
-	};
+	//}
+	T m_x;
+	T m_y;
+	T m_z;
+};
 
-	template <typename T>
-	class TMyTransform
-	{
-	public:
-		TMyVector<T> m_translation;
-		TMyQuaternion<T> m_rotation;
-		TMyVector<T> m_scale;
-	};
+template <typename T>
+class TMyQuaternion
+{
+public:
+	//TMyQuaternion()
+	//	: m_x(0)
+	//	, m_y(0)
+	//	, m_z(0)
+	//	, m_w(0)
+	//{
 
-	template <typename T>
-	bool operator==(const TMyVector<T>& lhs, const TMyVector<T>& rhs)
-	{
-		return
-			lhs.m_x == rhs.m_x &&
-			lhs.m_y == rhs.m_y &&
-			lhs.m_z == rhs.m_z
-			;
-	}
-	template <typename T>
-	bool operator==(const TMyQuaternion<T>& lhs, const TMyQuaternion<T>& rhs)
-	{
-		return
-			lhs.m_x == rhs.m_x &&
-			lhs.m_y == rhs.m_y &&
-			lhs.m_z == rhs.m_z &&
-			lhs.m_w == rhs.m_w
-			;
-	}
-	template <typename T>
-	bool operator==(const TMyTransform<T>& lhs, const TMyTransform<T>& rhs)
-	{
-		return
-			lhs.m_translation == rhs.m_translation &&
-			lhs.m_rotation == rhs.m_rotation &&
-			lhs.m_scale == rhs.m_scale
-			;
-	}
+	//}
+	T m_x;
+	T m_y;
+	T m_z;
+	T m_w;
+};
 
-	typedef TMyTransform<float> CMyTransformFloat;
+template <typename T>
+class TMyTransform
+{
+public:
+	TMyVector<T> m_translation;
+	TMyQuaternion<T> m_rotation;
+	TMyVector<T> m_scale;
+};
 
-	template <typename T>
-	static void TestInit_TMyTransform(TMyTransform<T>& srcData)
-	{
-		srcData.m_translation.m_x = 1.0f;
-		srcData.m_translation.m_y = 2.0f;
-		srcData.m_translation.m_z = 3.0f;
-		srcData.m_rotation.m_x = 4.0f;
-		srcData.m_rotation.m_y = 5.0f;
-		srcData.m_rotation.m_z = 6.0f;
-		srcData.m_rotation.m_w = 7.0f;
-		srcData.m_scale.m_x = 8.0f;
-		srcData.m_scale.m_y = 9.0f;
-		srcData.m_scale.m_z = 10.0f;
-	}
+template <typename T>
+bool operator==(const TMyVector<T>& lhs, const TMyVector<T>& rhs)
+{
+	return
+		lhs.m_x == rhs.m_x &&
+		lhs.m_y == rhs.m_y &&
+		lhs.m_z == rhs.m_z
+		;
+}
+template <typename T>
+bool operator==(const TMyQuaternion<T>& lhs, const TMyQuaternion<T>& rhs)
+{
+	return
+		lhs.m_x == rhs.m_x &&
+		lhs.m_y == rhs.m_y &&
+		lhs.m_z == rhs.m_z &&
+		lhs.m_w == rhs.m_w
+		;
+}
+template <typename T>
+bool operator==(const TMyTransform<T>& lhs, const TMyTransform<T>& rhs)
+{
+	return
+		lhs.m_translation == rhs.m_translation &&
+		lhs.m_rotation == rhs.m_rotation &&
+		lhs.m_scale == rhs.m_scale
+		;
+}
 
-	template <typename T>
-	Niflect::CString MyVectorToString(const TMyVector<T>& v)
-	{
-		std::basic_stringstream<Niflect::CString::value_type, Niflect::CString::traits_type, Niflect::CString::allocator_type> ss;
-		ss << v.m_x << ',' << v.m_y << ',' << v.m_z;
-		return ss.str();
-	}
-	template <typename T>
-	TMyVector<T> StringToMyVector(const Niflect::CString& str)
-	{
-		std::basic_stringstream<Niflect::CString::value_type, Niflect::CString::traits_type, Niflect::CString::allocator_type> ss(str);
-		TMyVector<T> v;
-		char d;
-		ss >> v.m_x >> d >> v.m_y >> d >> v.m_z;
-		return v;
-	}
+typedef TMyTransform<float> CMyTransformFloat;
 
-	template <typename T>
-	Niflect::CString MyQuaternionToString(const TMyQuaternion<T>& v)
-	{
-		std::basic_stringstream<Niflect::CString::value_type, Niflect::CString::traits_type, Niflect::CString::allocator_type> ss;
-		ss << v.m_x << ',' << v.m_y << ',' << v.m_z << ',' << v.m_w;
-		return ss.str();
-	}
-	template <typename T>
-	TMyQuaternion<T> StringToMyQuaternion(const Niflect::CString& str)
-	{
-		std::basic_stringstream<Niflect::CString::value_type, Niflect::CString::traits_type, Niflect::CString::allocator_type> ss(str);
-		TMyQuaternion<T> v;
-		char d;
-		ss >> v.m_x >> d >> v.m_y >> d >> v.m_z >> d >> v.m_w;
-		return v;
-	}
+template <typename T>
+static void TestInit_TMyTransform(TMyTransform<T>& srcData)
+{
+	srcData.m_translation.m_x = 1.0f;
+	srcData.m_translation.m_y = 2.0f;
+	srcData.m_translation.m_z = 3.0f;
+	srcData.m_rotation.m_x = 4.0f;
+	srcData.m_rotation.m_y = 5.0f;
+	srcData.m_rotation.m_z = 6.0f;
+	srcData.m_rotation.m_w = 7.0f;
+	srcData.m_scale.m_x = 8.0f;
+	srcData.m_scale.m_y = 9.0f;
+	srcData.m_scale.m_z = 10.0f;
+}
+
+template <typename T>
+Niflect::CString MyVectorToString(const TMyVector<T>& v)
+{
+	std::basic_stringstream<Niflect::CString::value_type, Niflect::CString::traits_type, Niflect::CString::allocator_type> ss;
+	ss << v.m_x << ',' << v.m_y << ',' << v.m_z;
+	return ss.str();
+}
+template <typename T>
+TMyVector<T> StringToMyVector(const Niflect::CString& str)
+{
+	std::basic_stringstream<Niflect::CString::value_type, Niflect::CString::traits_type, Niflect::CString::allocator_type> ss(str);
+	TMyVector<T> v;
+	char d;
+	ss >> v.m_x >> d >> v.m_y >> d >> v.m_z;
+	return v;
+}
+
+template <typename T>
+Niflect::CString MyQuaternionToString(const TMyQuaternion<T>& v)
+{
+	std::basic_stringstream<Niflect::CString::value_type, Niflect::CString::traits_type, Niflect::CString::allocator_type> ss;
+	ss << v.m_x << ',' << v.m_y << ',' << v.m_z << ',' << v.m_w;
+	return ss.str();
+}
+template <typename T>
+TMyQuaternion<T> StringToMyQuaternion(const Niflect::CString& str)
+{
+	std::basic_stringstream<Niflect::CString::value_type, Niflect::CString::traits_type, Niflect::CString::allocator_type> ss(str);
+	TMyQuaternion<T> v;
+	char d;
+	ss >> v.m_x >> d >> v.m_y >> d >> v.m_z >> d >> v.m_w;
+	return v;
 }
 
 namespace Engine
@@ -331,6 +343,8 @@ namespace Engine
 
 namespace TestAccessor
 {
+	using namespace Engine;
+
 	class CTestClassMy
 	{
 	public:
@@ -359,6 +373,7 @@ namespace TestAccessor
 			m_array_array_4[1].resize(2);
 			m_array_array_4[1][0] = 1.5f;
 			m_array_array_4[1][1] = 1.6f;
+			TestInit_TMyTransform(m_tm_5);
 		}
 
 		bool operator==(const CTestClassMy& rhs) const
@@ -368,7 +383,8 @@ namespace TestAccessor
 				(m_array_1 == rhs.m_array_1) &&
 				(m_float_2 == rhs.m_float_2) &&
 				(m_bool_3 == rhs.m_bool_3) &&
-				(m_array_array_4 == rhs.m_array_array_4)
+				(m_array_array_4 == rhs.m_array_array_4) &&
+				(m_tm_5 == rhs.m_tm_5)
 				;
 		}
 
@@ -377,6 +393,7 @@ namespace TestAccessor
 		float m_float_2;
 		bool m_bool_3;
 		Niflect::TArrayNif<Niflect::TArrayNif<float> > m_array_array_4;
+		TMyTransform<float> m_tm_5;
 	};
 
 	class CTestClassMy2 : public CTestClassMy
@@ -406,6 +423,13 @@ namespace TestAccessor
 			m_derived_array_3[1] = false;
 			m_derived_array_3[2] = true;
 			m_derived_bool_4 = false;
+			m_derived_array_5.resize(3);
+			for (auto idx = 0; idx < m_derived_array_5.size(); ++idx)
+			{
+				auto& it = m_derived_array_5[idx];
+				TestInit_TMyTransform(it);
+				it.m_scale.m_y += 1 + idx;
+			}
 		}
 
 		bool operator==(const CTestClassMy2& rhs) const
@@ -417,7 +441,8 @@ namespace TestAccessor
 					(m_derived_array_1 == rhs.m_derived_array_1) &&
 					(m_derived_float_2 == rhs.m_derived_float_2) &&
 					(m_derived_array_3 == rhs.m_derived_array_3) &&
-					(m_derived_bool_4 == rhs.m_derived_bool_4)
+					(m_derived_bool_4 == rhs.m_derived_bool_4) &&
+					(m_derived_array_5 == rhs.m_derived_array_5)
 					;
 			}
 			return false;
@@ -428,6 +453,7 @@ namespace TestAccessor
 		float m_derived_float_2;
 		Niflect::TArrayNif<bool> m_derived_array_3;
 		bool m_derived_bool_4;
+		Niflect::TArrayNif<TMyTransform<float> > m_derived_array_5;
 	};
 
 	static Niflect::CSharedAccessor BuildAccessor_CTestClassMy()
@@ -475,6 +501,11 @@ namespace TestAccessor
 			}
 			accessor0->AddChild(accessor1);
 		}
+		{
+			auto accessor1 = Niflect::MakeShared<TMyTransformAccessor<float> >();
+			accessor1->InitMemberMeta("m_tm_5", Niflect::GetMemberVariableOffset(&CTestClassMy::m_tm_5));
+			accessor0->AddChild(accessor1);
+		}
 		return accessor0;
 	}
 	static Niflect::CSharedAccessor BuildAccessor_CTestClassMy2()
@@ -515,6 +546,16 @@ namespace TestAccessor
 		{
 			auto accessor1 = Niflect::MakeShared<CBoolAccessor>();
 			accessor1->InitMemberMeta("m_derived_bool_4", Niflect::GetMemberVariableOffset(&CTestClassMy2::m_derived_bool_4));
+			accessor0->AddChild(accessor1);
+		}
+		{
+			auto accessor1 = Niflect::MakeShared<TStlArrayAccessor<Niflect::TArrayNif<TMyTransform<float> > > >();
+			accessor1->InitMemberMeta("m_derived_array_5", Niflect::GetMemberVariableOffset(&CTestClassMy2::m_derived_array_5));
+			{
+				auto accessor2 = Niflect::MakeShared<TMyTransformAccessor<float> >();
+				accessor2->InitMemberMeta("reserved_dim0", Niflect::CAddrOffset::None);
+				accessor1->AddChild(accessor2);
+			}
 			accessor0->AddChild(accessor1);
 		}
 		return accessor0;
