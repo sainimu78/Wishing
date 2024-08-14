1544fee29afafd2b6f326877ef3b2dbe93811fde
sainimu78 <393584307@qq.com>
2024-08-14 14:19:00 +0800

    -feature:预留平台case

--- Project/CommonAfterProject.cmake
+++ Project/CommonAfterProject.cmake
@@ -3,12 +3,20 @@
 
 # CMAKE_CXX_COMPILER 略特殊, 默认无设置时, 需要在 project 命令后执行才具有检测到的默认路径 Compiler 
 if(CMAKE_CXX_COMPILER MATCHES "c\\+\\+$")
-	message(STATUS "Using GCC")
+	message(STATUS "Compiler Is Using GCC")
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-format-zero-length")
 elseif(CMAKE_CXX_COMPILER MATCHES "clang\\+\\+$")
-	message(STATUS "Using Clang")
+	message(STATUS "Compiler Is Using Clang")
 	#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-format-zero-length")
 elseif(MSVC)
-	message(STATUS "Using MSVC")
+	message(STATUS "Compiler Is Using MSVC")
 	#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4")
+endif()
+
+if(UNIX)
+	message(STATUS "Target Is on UNIX")
+elseif(APPLE)
+	message(STATUS "Target Is on APPLE")
+elseif(WIN32)
+	message(STATUS "Target Is on WIN32")
 endif()
