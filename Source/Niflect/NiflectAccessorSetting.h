838a0a23d6fc3979c13538af02217e95a983a46a
sainimu78 <393584307@qq.com>
2024-10-20 00:09:59 +0800

    简化标记名称

--- Source/Niflect/NiflectAccessorSetting.h
+++ Source/Niflect/NiflectAccessorSetting.h
@@ -65,17 +65,17 @@
 //2. 结构模板(Structural Template), 如 std::pair
 //此设置在模块范围内只可使用1次
 //名称末尾的1表示 TSetting 模板需要指定的模板参数个数为1
-#define NIF_AS_Compound_1() using NIFLECTGENINTERNAL_COMBINE(__NiflectGen_AccessorSetting_Compound,_,__LINE__) = 
+#define NIF_AS_Compound_1() using NIFLECTGENINTERNAL_COMBINE(__NiflectGen_AccessorSettingA,_,__LINE__) = 
 
 //enum class 将绑定此设置指定的 Accessor
 //此设置在模块范围内只可使用1次
 //名称末尾的1表示 TSetting 模板需要指定的模板参数个数为1
-#define NIF_AS_EnumClass_1() using NIFLECTGENINTERNAL_COMBINE(__NiflectGen_AccessorSetting_EnumClass,_,__LINE__) = 
+#define NIF_AS_EnumClass_1() using NIFLECTGENINTERNAL_COMBINE(__NiflectGen_AccessorSettingB,_,__LINE__) = 
 
 //enum 将绑定此设置指定的 Accessor
 //此设置在模块范围内只可使用1次
 //名称末尾的1表示 TSetting 模板需要指定的模板参数个数为1
-#define NIF_AS_EnumBitsMask_1() using NIFLECTGENINTERNAL_COMBINE(__NiflectGen_AccessorSetting_EnumBitsMask,_,__LINE__) = 
+#define NIF_AS_EnumBitsMask_1() using NIFLECTGENINTERNAL_COMBINE(__NiflectGen_AccessorSettingC,_,__LINE__) = 
 
 //可被绑定为此设置指定 Accessor 的类型有
 //1. 容器模板(Container Template), 如 std::vector, std::map
@@ -83,7 +83,7 @@
 //3. 特化模板, 如 std::vector<bool>
 //此设置在模块范围内可多次使用, 以指定不同的类型的 Accessor 或覆盖其它设置指定的 Accessor
 //名称末尾的2和3表示 TSetting 模板需要指定的模板参数个数为2或3
-#define NIF_AS_Binding_2_3() using NIFLECTGENINTERNAL_COMBINE(__NiflectGen_AccessorSetting_Binding,_,__LINE__) = 
+#define NIF_AS_Binding_2_3() using NIFLECTGENINTERNAL_COMBINE(__NiflectGen_AccessorSettingD,_,__LINE__) = 
 
 namespace NiflectAccessorSetting
 {
