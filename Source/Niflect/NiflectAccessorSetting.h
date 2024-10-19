525cd522d48c64b93a98a17954411c6a4cc1f394
sainimu78 <393584307@qq.com>
2024-10-20 00:50:08 +0800

    规则调整

--- Source/Niflect/NiflectAccessorSetting.h
+++ Source/Niflect/NiflectAccessorSetting.h
@@ -60,30 +60,36 @@
 #define NIFLECTGENINTERNAL_COMBINE_INNER(A,B,C) A##B##C
 #define NIFLECTGENINTERNAL_COMBINE(A,B,C) NIFLECTGENINTERNAL_COMBINE_INNER(A,B,C)
 
+//General Accessor 设置, 简称 A 型设置
 //可被绑定为此设置指定 Accessor 的类型有
-//1. 带标记 NIF_T 的类型, 如 class, struct
-//2. 结构模板(Structural Template), 如 std::pair
-//此设置在模块范围内只可使用1次
-//名称末尾的1表示 TSetting 模板需要指定的模板参数个数为1
-#define NIF_AS_Compound_1() using NIFLECTGENINTERNAL_COMBINE(__NiflectGen_AccessorSettingA,_,__LINE__) = 
-
-//enum class 将绑定此设置指定的 Accessor
-//此设置在模块范围内只可使用1次
-//名称末尾的1表示 TSetting 模板需要指定的模板参数个数为1
-#define NIF_AS_EnumClass_1() using NIFLECTGENINTERNAL_COMBINE(__NiflectGen_AccessorSettingB,_,__LINE__) = 
-
-//enum 将绑定此设置指定的 Accessor
-//此设置在模块范围内只可使用1次
-//名称末尾的1表示 TSetting 模板需要指定的模板参数个数为1
-#define NIF_AS_EnumBitsMask_1() using NIFLECTGENINTERNAL_COMBINE(__NiflectGen_AccessorSettingC,_,__LINE__) = 
-
-//可被绑定为此设置指定 Accessor 的类型有
-//1. 容器模板(Container Template), 如 std::vector, std::map
-//2. 所有能被 TCompount, TEnumClass, TEnumBitsMask 绑定的类型
+//1. 容器模板(Container Template 1D), 如 std::vector, std::map
+//2. 结构模板(Structural Template nD, n>=2), 如 std::pair
 //3. 特化模板, 如 std::vector<bool>
+//4. 所有能被其它类型设置所绑定的类型, 因此, A 型设置也可理解为一种覆盖设置
 //此设置在模块范围内可多次使用, 以指定不同的类型的 Accessor 或覆盖其它设置指定的 Accessor
-//名称末尾的2和3表示 TSetting 模板需要指定的模板参数个数为2或3
-#define NIF_AS_Binding_2_3() using NIFLECTGENINTERNAL_COMBINE(__NiflectGen_AccessorSettingD,_,__LINE__) = 
+//TSetting 模板需要指定的模板参数个数为2或3
+#define NIF_AS_A() using NIFLECTGENINTERNAL_COMBINE(__NiflectGen_AccessorSetting_A,_,__LINE__) = 
+
+//Compound Accessor 设置, 简称 B 型设置
+//带标记 NIF_T 的类型, 如 class, struct
+//当未指定类型的 A 型设置时, 则类型绑定此设置指定的 Accessor
+//此设置在模块范围内只可使用1次
+//TSetting 模板需要指定的模板参数个数为1
+#define NIF_AS_B() using NIFLECTGENINTERNAL_COMBINE(__NiflectGen_AccessorSetting_B,_,__LINE__) = 
+
+//Enum Class Accessor 设置, 简称 C 型设置
+//enum class 将绑定此设置指定的 Accessor
+//当未指定类型的 A 型设置时, 则类型绑定此设置指定的 Accessor
+//此设置在模块范围内只可使用1次
+//TSetting 模板需要指定的模板参数个数为1
+#define NIF_AS_C() using NIFLECTGENINTERNAL_COMBINE(__NiflectGen_AccessorSetting_C,_,__LINE__) = 
+
+//Enum Bits Mask Accessor 设置, 简称 D 型设置
+//enum 将绑定此设置指定的 Accessor
+//当未指定类型的 A 型设置时, 则类型绑定此设置指定的 Accessor
+//此设置在模块范围内只可使用1次
+//TSetting 模板需要指定的模板参数个数为1
+#define NIF_AS_D() using NIFLECTGENINTERNAL_COMBINE(__NiflectGen_AccessorSetting_D,_,__LINE__) = 
 
 namespace NiflectAccessorSetting
 {
