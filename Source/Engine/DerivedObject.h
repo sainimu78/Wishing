c83236d616bbf8608c4fdaec33e53e1d33e0a704
sainimu78 <393584307@qq.com>
2024-11-03 15:41:17 +0800

    加测试成员

--- Source/Engine/DerivedObject.h
+++ Source/Engine/DerivedObject.h
@@ -8,20 +8,33 @@ namespace Engine
 	{
 		typedef CEngineObject inherited;
 	public:
+		CDerivedObject()
+			: m_derived_bool_1(false)
+			, m_derived_float_2(0.0f)
+		{
+		}
 		void InitForTest()
 		{
 			inherited::InitForTest();
 			m_derived_string_0 = "derived nihao";
+			m_derived_bool_1 = true;
+			m_derived_float_2 = 11.0f;
 		}
 		bool operator==(const CDerivedObject& rhs) const
 		{
 			return inherited::operator==(rhs)
 				&& m_derived_string_0 == rhs.m_derived_string_0
+				&& m_derived_bool_1 == rhs.m_derived_bool_1
+				&& m_derived_float_2 == rhs.m_derived_float_2
 				;
 		}
 	public:
 		NIF_F()
 		Niflect::CString m_derived_string_0;
+		NIF_F()
+		bool m_derived_bool_1;
+		NIF_F()
+		float m_derived_float_2;
 	};
 
 	//NIF_T()
