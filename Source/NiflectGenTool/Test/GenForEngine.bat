a21dfb9e50a548f2bb796eb4c37c7ef442891d10
sainimu78 <393584307@qq.com>
2024-11-09 21:09:39 +0800

    测试生成bat

---
+++ Source/NiflectGenTool/Test/GenForEngine.bat
@@ -0,0 +1,10 @@
+cd ../../../Build/NiflectGenTool/Windows/vs2022_NiflectGenTool_x64/Debug/NiflectGenTool
+NiflectGenTool \
+-n Engine \
+-h F:/Fts/Proj/Test/Interedit/Source/Engine/EngineObject.h \
+-h F:/Fts/Proj/Test/Interedit/Source/Engine/DerivedObject.h \
+-am ENGINE_API \
+-amh F:/Fts/Proj/Test/Interedit/Source/Engine/EngineCommon.h \
+-a F:/Fts/Proj/Test/Interedit/Source/Engine/EngineAccessorSetting.h \
+-I F:/Fts/Proj/Test/Interedit/Source \
+-p F:/Fts/Proj/Test/Interedit/Generated/NiflectGenerated \
