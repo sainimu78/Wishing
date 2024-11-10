ffdb9d37ea75c7188657c3d02c3980b5e27acd01
sainimu78 <393584307@qq.com>
2024-11-10 18:00:49 +0800

    field标记改用行号宏定义符号, 避免 linux 下 libclang解析报重定义 __NiflectGen_Field 的错误

--- Source/Niflect/NiflectMacro.h
+++ Source/Niflect/NiflectMacro.h
@@ -5,7 +5,7 @@
 #define GENERATED_BODY(...) typedef void* __NiflectGen_GeneratedBody;
 
 #define NIFLECTGENTAG_TYPE typedef void* __NiflectGen_Type;
-#define NIFLECTGENTAG_FIELD typedef void* __NiflectGen_Field;
+#define NIFLECTGENTAG_FIELD typedef void* CONCAT_SYMBOLS_2(__NiflectGen_Field,__LINE__);
 #define NIFLECTGENTAG_METHOD typedef void* __NiflectGen_Method;
 #define NIFLECTGENTAG_CONSTANT __attribute__((annotate("__NiflectGen_EnumConstant")))
 #else
