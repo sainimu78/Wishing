a6763af1c762e0d19477327012dcd03b2a9d43a3
sainimu78 <393584307@qq.com>
2024-11-10 17:58:19 +0800

    改用规范定义的符号拼接宏

--- Source/Niflect/NiflectMacro.h
+++ Source/Niflect/NiflectMacro.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Niflect/Util/ConcatSymbols.h"
 
 #ifdef NIFLECTGEN
 #define GENERATED_BODY(...) typedef void* __NiflectGen_GeneratedBody;
@@ -8,12 +9,10 @@
 #define NIFLECTGENTAG_METHOD typedef void* __NiflectGen_Method;
 #define NIFLECTGENTAG_CONSTANT __attribute__((annotate("__NiflectGen_EnumConstant")))
 #else
-#define BODY_MACRO_COMBINE_INNER(A,B,C,D) A##B##C##D
-#define BODY_MACRO_COMBINE(A,B,C,D) BODY_MACRO_COMBINE_INNER(A,B,C,D)
 
 // Include a redundant semicolon at the end of the generated code block, so that intellisense parsers can start parsing
 // a new declaration if the line number/generated code is out of date.
-#define GENERATED_BODY(...) BODY_MACRO_COMBINE(CURRENT_FILE_ID,_,__LINE__,_GENERATED_BODY);
+#define GENERATED_BODY(...) CONCAT_SYMBOLS_4(CURRENT_FILE_ID,_,__LINE__,_GENERATED_BODY);
 
 #define NIFLECTGENTAG_TYPE 
 #define NIFLECTGENTAG_FIELD 
