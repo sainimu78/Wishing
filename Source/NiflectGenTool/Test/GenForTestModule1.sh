776b83d67aa7899d52dd6cd4a81484ff2f69967c
sainimu78 <393584307@qq.com>
2024-11-11 19:04:38 +0800

    add -fs to test sh

---
+++ Source/NiflectGenTool/Test/GenForTestModule1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+old=$(pwd)
+cd ../../../Build/NiflectGenTool/Linux/Linux_NiflectGenTool_x64/Debug/NiflectGenTool
+
+./NiflectGenTool \
+-n TestModule1 \
+-h /home/sainimu78/Fts/Interedit/Source/TestModule1/TestClass1.h \
+-am TESTMODULE1_API \
+-amh /home/sainimu78/Fts/Interedit/Source/TestModule1/TestModule1Common.h \
+-a /home/sainimu78/Fts/Interedit/Source/Niflect/CommonlyUsed/DefaultAccessorSetting.h \
+-I /home/sainimu78/Fts/Interedit/Source \
+-p /home/sainimu78/Fts/Interedit/Generated/NiflectGenerated \
+-fs
+
+cd $old
