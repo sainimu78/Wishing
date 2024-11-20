a1c6df0c2a2690aa70d1cb8b8b796c1638b09caf
sainimu78 <393584307@qq.com>
2024-11-21 03:52:14 +0800

    use the new inc path

--- Source/NiflectGenTool/Test/GenForEngine.sh
+++ Source/NiflectGenTool/Test/GenForEngine.sh
@@ -11,6 +11,7 @@ cd ../../../Build/NiflectGenTool/Linux/Make_x64/Debug/NiflectGenTool
 -amh /home/sainimu78/Fts/Interedit/Source/Engine/EngineCommon.h \
 -a /home/sainimu78/Fts/Interedit/Source/Engine/EngineAccessorSetting.h \
 -I /home/sainimu78/Fts/Interedit/Source \
+-I /home/sainimu78/Fts/Interedit/Source/Niflect/include \
 -g /home/sainimu78/Fts/Interedit/Generated/NiflectGenerated 
 
 cd $old
--- Source/NiflectGenTool/Test/GenForTestModule1.sh
+++ Source/NiflectGenTool/Test/GenForTestModule1.sh
@@ -8,8 +8,9 @@ cd ../../../Build/NiflectGenTool/Linux/Make_x64/Debug/NiflectGenTool
 -h /home/sainimu78/Fts/Interedit/Source/TestModule1/TestClass1.h \
 -am TESTMODULE1_API \
 -amh /home/sainimu78/Fts/Interedit/Source/TestModule1/TestModule1Common.h \
--a /home/sainimu78/Fts/Interedit/Source/Niflect/CommonlyUsed/DefaultAccessorSetting.h \
+-a /home/sainimu78/Fts/Interedit/Source/Niflect/include/Niflect/CommonlyUsed/DefaultAccessorSetting.h \
 -I /home/sainimu78/Fts/Interedit/Source \
+-I /home/sainimu78/Fts/Interedit/Source/Niflect/include \
 -g /home/sainimu78/Fts/Interedit/Generated/NiflectGenerated 
 
 cd $old
