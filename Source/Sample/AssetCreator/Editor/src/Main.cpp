a92b18ff5ef8e1f6243423c15341356badfc87ba
sainimu78 <393584307@qq.com>
2024-12-17 22:26:40 +0800

    关闭控制台

--- Source/Sample/AssetCreator/Editor/src/Main.cpp
+++ Source/Sample/AssetCreator/Editor/src/Main.cpp
@@ -1,7 +1,11 @@
 #include <QApplication>
 #include <QPushButton>
+#ifdef WIN32
+#include <Windows.h>
+#endif
 
-int main(int argc, char* argv[]) {
+int EditorMain(int argc, char** argv)
+{
     QApplication app(argc, argv);
 
     QPushButton button("Hello, Qt!");
@@ -10,3 +14,33 @@ int main(int argc, char* argv[]) {
 
     return app.exec();
 }
+
+#ifdef WIN32
+static char* CreateCharsFromWChars(int codePage, const wchar_t* aw)
+{
+    const int required = WideCharToMultiByte(codePage, 0, aw, -1, NULL, 0, NULL, NULL);
+    char* result = new char[required];
+    WideCharToMultiByte(codePage, 0, aw, -1, result, required, NULL, NULL);
+    return result;
+}
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    int argc;
+    wchar_t** argvW = CommandLineToArgvW(GetCommandLineW(), &argc);
+    if (!argvW)
+        return -1;
+    char** argv = new char*[argc + 1];
+    for (int idx = 0; idx < argc; ++idx)
+        argv[idx] = CreateCharsFromWChars(CP_ACP, argvW[idx]);
+    argv[argc] = 0;
+    EditorMain(argc, argv);
+    for (int idx = 0; idx < argc; ++idx)
+        delete[] argv[idx];
+    delete[] argv;
+}
+#else
+int main(int argc, char** argv)
+{
+    EditorMain(argc, argv);
+}
+#endif
