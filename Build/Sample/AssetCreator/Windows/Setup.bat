f80ffb0759885d76222ba19d24f0be813de2d6cd
sainimu78 <393584307@qq.com>
2024-12-28 19:32:35 +0800

    改7z路径

--- Build/Sample/AssetCreator/Windows/Setup.bat
+++ Build/Sample/AssetCreator/Windows/Setup.bat
@@ -12,8 +12,9 @@ set Password=1
 set StorageDirPath=http://192.168.245.158/sainimu78_Storage
 
 set Name7zExe=7za.exe
+set Storage7zDirPath=%StorageDirPath%/Tool/%Platform%/7z
 set Dst7zExeFilePath=%ToolDirPath%\%Name7zExe%
-curl -u %UserName%:%Password% -L -o "%Dst7zExeFilePath%" "%StorageDirPath%/Tool/%Platform%/%Name7zExe%"
+curl -u %UserName%:%Password% -L -o "%Dst7zExeFilePath%" "%Storage7zDirPath%/%Name7zExe%"
 
 set DependencyName_T0T=Niflect
 set SrcRootDirPath_T0T=%StorageDirPath%/Release/%Platform%
@@ -24,7 +25,7 @@ set SrcZipFilePath_T0T=%SrcRootDirPath_T0T%/%ZipFileName_T0T%
 set DstZipFilePath_T0T=%DstRootDirPath_T0T%\%ZipFileName_T0T%
 curl -u %UserName%:%Password% -L -o "%DstZipFilePath_T0T%" "%SrcZipFilePath_T0T%"
 rmdir /s /q "%SrcExtractedDirPath_T0T%"
-7za x "%DstZipFilePath_T0T%" -o"%DstRootDirPath_T0T%"
+%Name7zExe% x "%DstZipFilePath_T0T%" -o"%DstRootDirPath_T0T%"
 del "%DstZipFilePath_T0T%"
 
 set DependencyName_T1T=Qt_5_8_0
@@ -36,7 +37,7 @@ set SrcZipFilePath_T1T=%SrcRootDirPath_T1T%/%ZipFileName_T1T%
 set DstZipFilePath_T1T=%DstRootDirPath_T1T%\%ZipFileName_T1T%
 curl -u %UserName%:%Password% -L -o "%DstZipFilePath_T1T%" "%SrcZipFilePath_T1T%"
 rmdir /s /q "%SrcExtractedDirPath_T1T%"
-7za x "%DstZipFilePath_T1T%" -o"%DstRootDirPath_T1T%"
+%Name7zExe% x "%DstZipFilePath_T1T%" -o"%DstRootDirPath_T1T%"
 del "%DstZipFilePath_T1T%"
 
 del "%Dst7zExeFilePath%"
--- Build/TestEditorCLI/Windows/Setup.bat
+++ Build/TestEditorCLI/Windows/Setup.bat
@@ -13,8 +13,9 @@ set StorageDirPath=http://192.168.245.158/sainimu78_Storage
 set SrcRootDirPath=%StorageDirPath%/Release/%Platform%
 
 set Name7zExe=7za.exe
+set Storage7zDirPath=%StorageDirPath%/Tool/%Platform%/7z
 set Dst7zExeFilePath=%ToolDirPath%\%Name7zExe%
-curl -u %UserName%:%Password% -L -o "%Dst7zExeFilePath%" "%StorageDirPath%/Tool/%Platform%/%Name7zExe%"
+curl -u %UserName%:%Password% -L -o "%Dst7zExeFilePath%" "%Storage7zDirPath%/%Name7zExe%"
 
 set DependencyName_T0T=Niflect
 set DstRootDirPath_T0T=%cd%\..\..\..\ThirdParty\%DependencyName_T0T%
@@ -24,7 +25,7 @@ set SrcZipFilePath_T0T=%SrcRootDirPath%/%ZipFileName_T0T%
 set DstZipFilePath_T0T=%DstRootDirPath_T0T%\%ZipFileName_T0T%
 curl -u %UserName%:%Password% -L -o "%DstZipFilePath_T0T%" "%SrcZipFilePath_T0T%"
 rmdir /s /q "%SrcExtractedDirPath_T0T%"
-7za x "%DstZipFilePath_T0T%" -o"%DstRootDirPath_T0T%"
+%Name7zExe% x "%DstZipFilePath_T0T%" -o"%DstRootDirPath_T0T%"
 del "%DstZipFilePath_T0T%"
 
 set DependencyName_T1T=NiflectGenTool
@@ -35,7 +36,7 @@ set SrcZipFilePath_T1T=%SrcRootDirPath%/%ZipFileName_T1T%
 set DstZipFilePath_T1T=%DstRootDirPath_T1T%\%ZipFileName_T1T%
 curl -u %UserName%:%Password% -L -o "%DstZipFilePath_T1T%" "%SrcZipFilePath_T1T%"
 rmdir /s /q "%SrcExtractedDirPath_T1T%"
-7za x "%DstZipFilePath_T1T%" -o"%DstRootDirPath_T1T%"
+%Name7zExe% x "%DstZipFilePath_T1T%" -o"%DstRootDirPath_T1T%"
 del "%DstZipFilePath_T1T%"
 
 del "%Dst7zExeFilePath%"
