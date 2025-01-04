823ee8df38b02b35e1ebd64212ddd78e86a2e537
sainimu78 <393584307@qq.com>
2025-01-04 23:15:37 +0800

    修复重复下载依赖

--- Build/Sample/AssetCreator/Linux/Setup.sh
+++ Build/Sample/AssetCreator/Linux/Setup.sh
@@ -6,11 +6,6 @@ InstallPrefix=../Installed
 Toolset="Unix Makefiles"
 OldDirPath=$(pwd)
 
-mkdir -p $BuildDirPathDebug
-cd $BuildDirPathDebug
-cmake $ProjectDirPath -G "$Toolset" -DCMAKE_INSTALL_PREFIX="$InstallPrefix" -DPROJECT_SETUP=ON -DPROJECT_RELEASE=OFF -DCMAKE_BUILD_TYPE=Debug
-cd $OldDirPath
-
 mkdir -p $BuildDirPathRelease
 cd $BuildDirPathRelease
 cmake $ProjectDirPath -G "$Toolset" -DCMAKE_INSTALL_PREFIX="$InstallPrefix" -DPROJECT_SETUP=ON -DPROJECT_RELEASE=OFF -DCMAKE_BUILD_TYPE=Release
