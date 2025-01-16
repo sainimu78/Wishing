5818931b7e48d484a8dada5f4bdca81eb29f0709
sainimu78 <393584307@qq.com>
2025-01-17 01:48:34 +0800

    修复名称错误

--- Project/Sample/TestHotReloading/IntegrateCreator.cmake
+++ Project/Sample/TestHotReloading/IntegrateCreator.cmake
@@ -1,7 +1,7 @@
 if(MSVC)
 	get_filename_component(ExeDirPathDebug "../../../../Build/Sample/AssetCreator/Windows/DefaultBuild/Debug/bin" ABSOLUTE)
 	get_filename_component(ExeDirPathRelease "../../../../Build/Sample/AssetCreator/Windows/DefaultBuild/Release/bin" ABSOLUTE)
-	set(ExeFileName Creator.exe)
+	set(ExeFileName Editor.exe)
 	set_target_properties(${ModuleName} PROPERTIES
 		VS_DEBUGGER_WORKING_DIRECTORY "$<IF:$<CONFIG:Debug>,${ExeDirPathDebug},${ExeDirPathRelease}>"
 	)
