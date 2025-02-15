58d9a682ca57342e0b395636bb5637b7c7ba1f5a
sainimu78 <393584307@qq.com>
2025-02-16 02:39:22 +0800

    避免依赖 .cmake 同级路径导致其它 Project 无法复用

--- Project/Sample/ContentCreator/WishingEngine/Licensed_boost.cmake
+++
@@ -1,6 +0,0 @@
-#if(WIN32)
-#	# #include "boost/asio.hpp" requires
-#	target_compile_definitions(${ModuleName} PRIVATE _WIN32_WINNT=0x0601)
-#    #add_definitions(-D_WIN32_WINNT=0x0601)
-#endif()
-include(${c_RootThirdPartyDirPath}/boost/boost_1_73_0/Static.cmake)
--- Project/WishingEngine/WishingEngine/Shared.cmake
+++ Project/WishingEngine/WishingEngine/Shared.cmake
@@ -41,7 +41,12 @@ target_compile_definitions(${ModuleName}
 
 include(${c_RootProjectDirPath}/Niflect/Niflect.cmake)
 
-include(Licensed_boost.cmake)
+#if(WIN32)
+#	# #include "boost/asio.hpp" requires
+#	target_compile_definitions(${ModuleName} PRIVATE _WIN32_WINNT=0x0601)
+#    #add_definitions(-D_WIN32_WINNT=0x0601)
+#endif()
+include(${c_RootThirdPartyDirPath}/boost/boost_1_73_0/Static.cmake)
 
 #target_link_libraries(${ModuleName} PRIVATE TestModule1)
 #include(${c_ProjectDirPath}/TestModule1/Shared.cmake)
