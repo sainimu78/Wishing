ff9e8f3b048410b2e2bfd0f05f6b11a2bb31b1e9
sainimu78 <393584307@qq.com>
2025-02-19 14:05:12 +0800

    备用

---
+++ Doc/b备用资料/VC最简构建示例/build.bat
@@ -0,0 +1,4 @@
+set INCLUDE=D:\HenDuoDongXi\Vs2022\VC\Tools\MSVC\14.33.31629\include;C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\ucrt
+set LIB=D:\HenDuoDongXi\Vs2022\VC\Tools\MSVC\14.33.31629\lib\x64;C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22621.0\um\x64;C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22621.0\ucrt\x64
+set PATH=D:\HenDuoDongXi\Vs2022\VC\Tools\MSVC\14.33.31629\bin\Hostx64\x64;%PATH%
+cl hello.cpp
