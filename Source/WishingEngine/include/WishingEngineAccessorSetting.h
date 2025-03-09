6188dd5c8481ec4006c97db6509b01efcb9fe429
sainimu78 <393584307@qq.com>
2025-03-10 01:03:13 +0800

    改类名

--- Source/WishingEngine/include/WishingEngineAccessorSetting.h
+++ Source/WishingEngine/include/WishingEngineAccessorSetting.h
@@ -1,8 +1,8 @@
 #pragma once
 #include "Niflect/Default/DefaultAccessorSetting.h"
-#include "WishingEngine/Base/PointerAccessor.h"
+#include "WishingEngine/Accessor/SharedPtrTypeAccessor.h"
 
 using namespace NiflectAccessorSetting;
 
 template<typename TInstance, typename T>
-NIF_AS_E() TSetting<Wishing::TSharedPtrAccessor<TInstance>, Niflect::TSharedPtr<T> >;
+NIF_AS_A() TSetting<Wishing::TSharedPtrTypeSpecialized<TInstance>, Niflect::TSharedPtr<T> >;
--- Source/WishingEngine/src/WishingEngine/Accessor/SharedPtrTypeAccessor.h
+++ Source/WishingEngine/src/WishingEngine/Accessor/SharedPtrTypeAccessor.h
@@ -1,37 +1,38 @@
 #pragma once
 #include "Niflect/NiflectAccessor.h"
-#include "Niflect/NiflectModuleManager.h"
+#include "Niflect/NiflectRegisteredType.h"
 
 namespace Wishing
 {
 	using namespace RwTree;
 
-	template <typename TSharedType>
-	class TSharedPtrAccessor : public Niflect::CAccessor
+	template <typename TPtr>
+	class TSharedPtrTypeSpecialized : public Niflect::CAccessor
 	{
+		using CSharedPtrType = Niflect::TSharedPtr<TPtr>;
 	protected:
 		virtual bool SaveInstanceImpl(const InstanceType* base, CRwNode* rw) const override
 		{
-			auto& instance = *static_cast<const TSharedType*>(base);
+			auto& instance = *static_cast<const CSharedPtrType*>(base);
 			if (instance == NULL)
 			{
-				AddRwBool(rw, "IsNULL", true);
+				AddRwBool(rw, "NULL", true);
 				return true;
 			}
-			auto type = instance->GetType();
+			auto type = Niflect::StaticGetType<TPtr>();
 			return type->SaveInstanceToRwNode(instance.Get(), rw);
 		}
 		virtual bool LoadInstanceImpl(InstanceType* base, const CRwNode* rw) const override
 		{
-			auto& instance = *static_cast<TSharedType*>(base);
-			bool isNULL = FindRwNode(rw, "IsNULL");
+			auto& instance = *static_cast<CSharedPtrType*>(base);
+			bool isNULL = FindRwBool(rw, "NULL");
 			if (isNULL)
 			{
 				instance = NULL;
 				return true;
 			}
-			auto type = instance
-			instance = Niflect::MakeShared<TInstanceType>();
+			auto type = Niflect::StaticGetType<TPtr>();
+			instance = Niflect::MakeShared<TPtr>();
 			return type->LoadInstanceFromRwNode(instance.Get(), rw);
 		}
 	};
